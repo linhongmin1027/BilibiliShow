a72ee3097986aa0abc0730ba4ab8dedacc972e42
linhongmin1027 <iOSDev@macbook-air.local>
2017-04-26 10:53:04 +0800

    Initial Commit

---
+++ BilibiliShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BilibiliShow
+//
+//  Created by iOSDev on 17/4/26.
+//  Copyright © 2017年 linhongmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BilibiliShow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BilibiliShow
+//
+//  Created by iOSDev on 17/4/26.
+//  Copyright © 2017年 linhongmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
